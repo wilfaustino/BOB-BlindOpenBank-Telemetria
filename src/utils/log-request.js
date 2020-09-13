4d675135fe790f463ef1bd64307ee45c6e8ad44f
wilfaustino <wilfaustino@gmail.com>
2020-09-13 18:17:30 -0300

    Modificado o log-request

--- src/utils/log-request.js
+++ src/utils/log-request.js
@@ -1,3 +1,41 @@
+const https = require('http');
 
-
-const url = 'http://ec2-18-218-87-145.us-east-2.compute.amazonaws.com:5001/logs';
+module.exports = function(){
+  
+    var options = {
+        host: 'ec2-18-218-87-145.us-east-2.compute.amazonaws.com', //base url
+        port: 5001, //port
+        path: '/logs', //API path
+        method: 'GET', //method
+        headers: {
+            'Authentication': 'bob-token-api'
+        }
+    };
+    
+    return new Promise(((resolve, reject) => {
+        
+        const request = https.request(options, (response) => {
+            
+            response.setEncoding('utf8');
+            
+            let returnData = '';
+            
+            response.on('data', (chunk) => {
+                returnData += chunk;
+            });
+            
+            response.on('end', () => {
+                resolve(JSON.parse(returnData));
+            });
+            
+            response.on('error', (error) => {
+                reject(error);
+            });
+            
+        });
+        
+        request.end();
+        
+    }));
+    
+}
